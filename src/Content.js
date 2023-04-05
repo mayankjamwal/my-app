5f9a2f85c8b9341a3af1ea664d64a82cee55e6f4
Mayank Jamwal <jamwalmayank75@gmail.com>
2023-04-05 19:36:55 +0530

    Added new styles

--- src/Content.js
+++ src/Content.js
@@ -1,7 +1,21 @@
 const Content = () => {
+    const handleNameChange = () =>{
+        const names = ['Bob','Kevin','Dave'];
+        const int = Math.floor(Math.random() * 3);
+        return names[int];
+    }
+
+    const handleClick = () =>{
+        console.log('You clicked it.');
+    }
+    const handleClick2 = (name) =>{
+        console.log(`${name} is passed.`);
+    }
     return (
         <main>
-            <p>Hello There!</p>
+            <p>Hello {handleNameChange()}!</p>
+            <button onClick={handleClick}>Click</button>
+            <button onClick={()=>handleClick2('Mayank')}>Click2</button>
         </main>
         
     )
